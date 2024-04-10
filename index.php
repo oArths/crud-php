f529f9d7fa3d6c623ad583e44a274aefd578f180
Arthur <arthurferreira1417@gmail.com>
2024-04-09 22:09:26 -0300

    New: home page

--- index.php
+++ index.php
@@ -3,30 +3,23 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Cadastro</title>
+    <title>Empresa</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
-    <div class="container">
+    <div class="container"> 
         <div class="row">
             <div class="col">
-            <h1>Cadastro!</h1>
-            <form>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+            <div class="jumbotron">
+                <h1 class="display-4">Castrastro</h1>
+                <p class="lead">Esse é um sistema simplificado de cadastro, Base de estudo para a criação de sistemas Web com PHP e MYSQL</p>
+                <hr class="my-4">
+                <p>Acessar funções</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="cadastro.php" role="button">Cadastro</a>
+                    <a class="btn btn-primary btn-lg" href="pesquisa.php" role="button">Pesquisa</a>
+                </p>
+                </div>
             </div>
         </div>
     </div>
