dbb54b8fedf653b4d22af39ab7d7b263a1a2d7fa
Arthur <arthurferreira1417@gmail.com>
2024-04-11 21:58:42 -0300

    Create: a login page

---
+++ restrito/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Empresa</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container"> 
+        <div class="row">
+            <div class="col">
+            <div class="jumbotron bg-body-tertiary p-5">
+                <h1 class="display-4">Castrastro Web</h1>
+                <p class="lead">Esse é um sistema simplificado de cadastro, Base de estudo para a criação de sistemas Web com PHP e MYSQL</p>
+                <hr class="my-4">
+                <p>Acessar funções</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="cadastro.php" role="button">Cadastro</a>
+                    <a class="btn btn-primary btn-lg" href="pesquisa.php" role="button">Pesquisa</a>
+                </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
