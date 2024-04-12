a9c8bb1aa6cb3c5383c793da2de787f11f56bb7c
Arthur <arthurferreira1417@gmail.com>
2024-04-12 19:57:34 -0300

    New: update

---
+++ validar.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+if(isset($_SESSION['login'])){
+    $user = $_SESSION['login'];
+}else{
+    session_destroy();
+    header("location: ../index.php?msg='voce foi expulso'");
+}
+?>
